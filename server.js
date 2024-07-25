2fd5157748fc25b0f37a9488ee9b7a98c984ca68
mansi.1788 <maithanimansi8@gmail.com>
2024-07-25 16:15:16 +0530

    second commit

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-let http=require("http")
-let fs=require("fs");
-console.log("starting the server")
-http.createServer((req,res)=>{
-   
-    fs.readFile('index.html',(err,data)=>{
-        if(!err){
-       res.writeHead(200,{
-        "Content-Type":"text/html",
-       });
-       res.write(data); //
-       return res.end();
-        }
-        res.write("error while reading"+err)
-        return res.end();
-    })
-})
-.listen(8080);
+const express = require("express");
+const app = express();
+const PORT =3000;
 
-//loop BACK ADDRESS.
+app.get("/",(req,res) => {
+    res.send("Hello world");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Example app listening on port ${PORT}`);
+})
